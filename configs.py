0fb94a27aadd038224404219ea56911e7aa85cc0
sonukumarkushvaha1-hub <sonukumarkushvaha1@gmail.com>
2026-05-02 02:48:17 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "410d526521ae7dcc474e0f4246788560")
     
     # 2. Bot Credentials (@BotFather se)
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8583460812:AAFg5QveoCDIzUhxytPQzgSpGfBDhGhw9s8")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "hdsssssa_bot")
     # Aapki Telegram User ID (Numeric)
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "5569039254"))
@@ -15,9 +15,9 @@ class Config(object):
     # Aapka naya clean URL jo aapne bheja tha
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sonukumarkushvaha1_db_user:Hwsb4hIkZ71eeApp@cluster0.2eu5lot.mongodb.net/?appName=Cluster0")
     # Storage Channel ID (Jahan files save hongi)
-    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1003789755565"))
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1003626737474"))
     # Database ka naam (Optional)
-    DB_NAME = os.environ.get("DB_NAME", "TheFilmClub_DB")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
     
     # 4. Force Subscribe Settings
     # The Film Club Channel ID
